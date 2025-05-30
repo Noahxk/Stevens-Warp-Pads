f98a1216138729c00106e0f051fcdf664d198836
Noahxk <noahxk@outlook.com>
2025-05-30 19:44:58 +1000

    Updated ServerboundWarpLocationSelectedPacket to contain needed information to teleport.

--- src/main/java/com/noahxk/stevenswarppads/network/payloads/ServerboundWarpLocationSelectedPacket.java
+++ src/main/java/com/noahxk/stevenswarppads/network/payloads/ServerboundWarpLocationSelectedPacket.java
@@ -7,14 +7,22 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 
-public record ServerboundWarpLocationSelectedPacket(Long fromPos, Long toPos) implements CustomPacketPayload {
+public record ServerboundWarpLocationSelectedPacket(long fromPos, String fromDimension, String fromId, long toPos, String toDimension, String toId) implements CustomPacketPayload {
     public static final CustomPacketPayload.Type<ServerboundWarpLocationSelectedPacket> TYPE = new CustomPacketPayload.Type<>(ResourceLocation.fromNamespaceAndPath(StevensWarpPads.MODID, "warplocationselected"));
 
     public static final StreamCodec<ByteBuf, ServerboundWarpLocationSelectedPacket> STREAM_CODEC = StreamCodec.composite(
             ByteBufCodecs.VAR_LONG,
             ServerboundWarpLocationSelectedPacket::fromPos,
+            ByteBufCodecs.STRING_UTF8,
+            ServerboundWarpLocationSelectedPacket::fromDimension,
+            ByteBufCodecs.STRING_UTF8,
+            ServerboundWarpLocationSelectedPacket::fromId,
             ByteBufCodecs.VAR_LONG,
             ServerboundWarpLocationSelectedPacket::toPos,
+            ByteBufCodecs.STRING_UTF8,
+            ServerboundWarpLocationSelectedPacket::toDimension,
+            ByteBufCodecs.STRING_UTF8,
+            ServerboundWarpLocationSelectedPacket::toId,
             ServerboundWarpLocationSelectedPacket::new
     );
 
